4dd5ba97cc3068739e1cdce44887e0871652a102
jiarun1 <liujiarun0529@gmail.com>
2023-11-16 23:31:23 +0000

    improve testing code

--- example/test_performance.cpp
+++ example/test_performance.cpp
@@ -32,17 +32,37 @@ public:
             std::cerr << "can't write to this file" << std::endl;
             return;
         }
-        file << "Line Num" << ' ' << "Time(us)" << ' ' << "Points Num" << " " << "Path Num" << " " << "Result Distance" << std::endl;
+        file << "Area Set" << ','
+             << "Line Num" << ',' 
+             << "Mapping Time(us)" << ',' 
+             << "Convertion Time(us)" << ',' 
+             << "Shorest Path Time(us)" << ',' 
+             << "Points Num" << "," 
+             << "Path Num" << ","
+             << "Result Distance" << std::endl;
     }
     ~writeLog_c()
     {
-
     }
 
-    void write(std::chrono::microseconds time_us, long long points_num, long long path_num, double min_dis)
+    void write(float set_area, 
+               std::chrono::microseconds mapping_time_us, 
+               std::chrono::microseconds convertion_time_us , 
+               std::chrono::microseconds shortestPath_time_us , 
+               long long points_num, 
+               long long path_num, 
+               double min_dis)
     {
+        file << set_area << "," 
+             << line_count << ',' 
+             << mapping_time_us.count() << ',' 
+             << convertion_time_us.count() << ','
+             << shortestPath_time_us.count() << ',' 
+             << points_num << "," 
+             << path_num << "," 
+             << min_dis << std::endl;
+
         line_count++;
-        file << line_count << ' ' << time_us.count() << ' ' << points_num << " " << path_num << " " << min_dis << std::endl;
     }
 
 };
@@ -53,46 +73,62 @@ int main(int argv, char** argc)
     // call the triangle with parameters
     triangleCommand triangle("../ThirdParty/triangle/triangle");
     
-    writeLog_c log("../tests/performance_test_1/log-1.txt");
+    writeLog_c log("../tests/performance_test_1/log-2.txt");
 
 
     float max_area = 20;
-    float area_dif = 1e-2;
+    float min_area = 1e-2;
+    float area_divide = 1.0/100.0+1;
+    int count_loop = 0;
 
     auto start_time = std::chrono::high_resolution_clock::now();
-    auto end_time = std::chrono::high_resolution_clock::now();
-    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end_time - start_time);
-
+    auto triangle_time = std::chrono::high_resolution_clock::now();
+    auto dataImportConvertion_time = std::chrono::high_resolution_clock::now();
+    auto shortestPathAlgorithm_time = std::chrono::high_resolution_clock::now();
 
     ProfilerStart("test.prof");
 
-    for(float test_area = max_area; test_area > 0; test_area -= area_dif)
+    for(float test_area = max_area; test_area > min_area; test_area = test_area / area_divide)
     {
+        count_loop ++;
+        if(count_loop >= 1000)
+        {
+            std::cout << count_loop << std::endl;
+            break;
+        }
+
         //-------------------------- Parameter Settings --------------------------------
         triangle.setParameter("a", test_area);
 
         //--------------------------- Start Count Time --------------------------------
         start_time = std::chrono::high_resolution_clock::now();
 
-
-
         triangle.call("../tests/performance_test_1/squareMapTest.poly");
+
+        triangle_time = std::chrono::high_resolution_clock::now();
         
+        // Data import and convertion start
         triangleData data;
         data.import("../tests/performance_test_1/squareMapTest.1.node", "../tests/performance_test_1/squareMapTest.1.ele");
 
         adjacencyMap map;
         data.toAdjacencyMap(&map);
 
+        dataImportConvertion_time = std::chrono::high_resolution_clock::now();
+        // Dijkstra algorithm start
+
         Dijkstra path_planing;
         path_planing.setAdjacencyMap(&map);
         path_planing.calculateShortestPath(1,3);
 
-        //--------------------------- End Count Time --------------------------------
-        end_time = std::chrono::high_resolution_clock::now();
-        duration = std::chrono::duration_cast<std::chrono::microseconds>(end_time - start_time);
+        shortestPathAlgorithm_time = std::chrono::high_resolution_clock::now();
 
-        log.write(duration, map.getPointNum(), map.getPathNum(), path_planing.getPathCost());
+        //--------------------------- End Count Time --------------------------------
+        auto mapping_duration = std::chrono::duration_cast<std::chrono::microseconds>(triangle_time - start_time);
+        auto convertion_duration = std::chrono::duration_cast<std::chrono::microseconds>(dataImportConvertion_time - triangle_time);
+        auto shortestPathAlgorithm_duration = std::chrono::duration_cast<std::chrono::microseconds>(shortestPathAlgorithm_time - dataImportConvertion_time);
+
+        log.write(test_area ,mapping_duration, convertion_duration ,shortestPathAlgorithm_duration, map.getPointNum(), map.getPathNum(), path_planing.getPathCost());
         
     }
     ProfilerStop();
