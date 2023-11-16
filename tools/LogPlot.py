b5e592d7fd56575617fc3c63b08b3daebf21bc7f
jiarun1 <liujiarun0529@gmail.com>
2023-11-16 20:31:25 +0000

    set font

--- tools/LogPlot.py
+++ tools/LogPlot.py
@@ -22,7 +22,7 @@ while 1:
     count = count + 1
     if line is None:
         break
-    if count == 1:
+    if count <= 100:
         continue
     if count >= 2000:
         break
@@ -46,29 +46,37 @@ line_num_max = max(line_num)
 
 ##################################################################33
 ########## Plot drawing 1 ##############
-plot.subplot(2,1,1)
-plot.scatter(point_num,cost_time)
+plot.figure(figsize=(21/2.54,15/2.54))
+plot.rc('font',family='Arial')
 
+plot.subplot(2,1,1)
+plot.scatter(point_num,cost_time,label="Test Result")
 # fit curve
 a,b,c = np.polyfit(point_num,cost_time,2)
 point_fit_result = [a*np.square(x)+b*x+c for x in point_num]
-plot.plot(point_num, point_fit_result, color="red")
-plot.title('Point Number Vs Execution Time')
-plot.legend(["Test Result", "Curve Fitted Result"])
-plot.xlabel('Point Number')
-plot.ylabel('Cost time ($ \mu s $)')
+fixed_function = str((a))+"$x^2$+" + str((b)) + "x+"+str((c))
+print(fixed_function)
+plot.plot(point_num, point_fit_result, color="red",label = "Curve Fitted Result")
+plot.title('Point Number Vs Execution Time',size=11)
+# plot.legend(["Test Result","Curve Fitted Result"])
+plot.legend()
+plot.xlabel('Point Number',size=11)
+plot.ylabel('Cost time ($ \mu s $)',size=11)
 plot.grid()
 
 plot.subplot(2,1,2)
-plot.scatter(line_num,cost_time)
+plot.scatter(line_num,cost_time,label="Test Result")
 a,b,c = np.polyfit(line_num,cost_time,2)
 line_fit_result = [a*np.square(x)+b*x+c for x in line_num]
-plot.plot(line_num, line_fit_result, color="red")
-plot.title('Line Number Vs Execution Time')
-plot.legend(["Test Result", "Curve Fitted Result"])
-plot.xlabel('Line Number')
-plot.ylabel('Cost time ($ \mu s $)')
+fixed_function = str((a))+"$x^2$+" + str((b)) + "x+"+str((c))
+print(fixed_function)
+plot.plot(line_num, line_fit_result, color="red",label = "Curve Fitted Result")
+plot.title('Line Number Vs Execution Time',size=11)
+plot.legend()
+plot.xlabel('Point Number',size=11)
+plot.ylabel('Cost time ($ \mu s $)',size=11)
 plot.grid()
+
 
 plot.tight_layout()
 plot.show()
